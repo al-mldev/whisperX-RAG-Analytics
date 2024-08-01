0a3a16ee0ebf0ab4c0a1f4f50f214e457d55e561
al-mldev <al.mldevel@gmail.com>
2024-08-01 15:53:16 -0400

    create collections script

--- db_utils/create_collections.py
+++ db_utils/create_collections.py
@@ -1,1 +1,15 @@
-#pending: insert collections schema
+from pymongo import MongoClient
+
+def create_collections(database_name):
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client[database_name]
+    collection_names = ["audio_files", "metrics" , "references", "transcriptions"]
+
+    for collection_name in collection_names:
+      
+        if collection_name in db.list_collection_names():
+            print(f"Collection '{collection_name}' already exist in '{database_name}'.")
+        else:
+            db.create_collection(collection_name)
+            print(f"Collection '{collection_name}' created in '{database_name}'.")
+
